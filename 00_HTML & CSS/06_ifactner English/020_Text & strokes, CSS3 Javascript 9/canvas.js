0166b592b4eb1e1137850544db089f80dbe51293
bogoreh <bogoreh026@gmail.com>
2021-01-07 10:49:11 +0300

    The first commit

---
+++ 00_HTML & CSS/06_ifactner English/020_Text & strokes, CSS3 Javascript 9/canvas.js
@@ -0,0 +1,8 @@
+var can=document.getElementById("Newcanvas");
+var Jctx=can.getContext("2d");
+Jctx.font= "40px Verdana";
+Jctx.fillStyle="Blue";
+Jctx.fillText("Abdibogoreh",50,250);
+
+Jctx.strokeStyle="Red";
+Jctx.strokeText("Habonita",75,150);
